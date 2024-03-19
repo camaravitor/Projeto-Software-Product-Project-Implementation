e52ea271aa192c67ea5ea350cb76ddcfdbd365bf
mbogres19 <theus.borges19@gmail.com>
2024-03-18 22:32:32 -0300

    Criando o banco

--- MySQL/script.sql
+++ MySQL/script.sql
@@ -1,7 +1,11 @@
 create database if not exists mv3;
 
+use mv3;
+
 create table if not exists diario (
-    dt_fech date,
-    val_fech decimal(20,4),
-    dt_ref date
+    vl_aber decimal(20,4)   comment 'Valor de abertura',
+    vl_max  decimal(20,4)   comment 'Valor máximo',
+    vl_min  decimal(20,4)   comment 'Valor minímo',
+    vl_fech decimal(20,4)   comment 'Valor de fechamento',
+    dt_fech date            comment 'Data de fechamento'
 );
